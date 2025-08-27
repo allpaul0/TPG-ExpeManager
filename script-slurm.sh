d7898cea5ce067fbaeb294296b1a3463241a63ed
Dardaillon Mickael <mickael.dardaillon@insa-rennes.fr>
2025-08-27 10:37:17 +0200

    Update slurm script.

--- script-slurm.sh
+++ script-slurm.sh
@@ -2,8 +2,8 @@
 #SBATCH --mem=10G
 #SBATCH --ntasks=1
 #SBATCH --job-name=testArmGegelati1
-#SBATCH --cpus-per-task=128
-#SBTACH --time=7200
+#SBATCH --cpus-per-task=48
+#SBTACH --time=28800
 EXPERIMENT_FOLDER=$1
 BIN_FOLDER=/armlearn-wrapper/build
 apptainer exec --bind $EXPERIMENT_FOLDER/params:/params/ --bind $EXPERIMENT_FOLDER/outLogs:/outLogs/ ./gegelati-arm.sif /bin/bash -c "cd / && .$BIN_FOLDER/armGegelati"
