9761642db0cca940a4ace80c3e8da6754fa5d425
allpaul0 <paulallaireperso@gmail.com>
2026-06-24 20:27:57 +0200

    plot training incomplete

--- plot-training.py
+++ plot-training.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-filename = "test.ods"
+filename = "../tpg_nextflow/tpg_fixedpt_comp_logexp_trigo_complete/training_results/useInstrTrig-True_useInstrLogExp-True_useInstrExpensiveArithmetic-True_useInstrComparison-True_seed-0_instrType-fixedpt/outLogs/garbage.ods"
 
 df = pd.read_csv(filename, sep=r"\s+", skiprows=1)
 
